ed6e7e4bb6a2f17479a2df8ae33eeab51f54f36c
Ian <ian@ichristoph.com>
2026-09-20 11:52:57 -0700

    Fix Apple icon artwork scaling (#59)

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -5,5 +5,5 @@ export const size = { width: 180, height: 180 };
 export const contentType = "image/png";
 
 export default function AppleIcon() {
-  return new ImageResponse(<AppIconArtwork />, size);
+  return new ImageResponse(<AppIconArtwork size={size.width} />, size);
 }
